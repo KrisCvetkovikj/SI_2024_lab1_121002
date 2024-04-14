aaa3d133757fd765781447bc07cde42f72454444
kristijan.cvetkovikj <kristijan.cvetkovikj@valtech.com>
2024-04-14 19:15:52 +0200

    Call method makeBigStringFrom in main class

--- SILab1Main.java
+++ SILab1Main.java
@@ -12,6 +12,7 @@ public class SILab1Main {
 
         for (int i = 0; i < n; i++) {
             System.out.println(SILab1_1.filterOddLengthStrings(list));
+            System.out.println(SILab1_2.makeBigStringFrom(list));
             list.add(sc.next());
         }
     }
